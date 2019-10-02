fd3e4450645eaeaacb4c83023cea766b6f572048
sinha104 <sinha104@umn.edu>
2019-10-01 21:59:32 -0500

    Complete Assignment 2 work. Now have a preview page visible

---
+++ pageData.js
@@ -0,0 +1,13 @@
+{
+    "pageData" = [
+        {
+            "me": "./The Wayback Machine/UMN_MainPage.html",
+            "orig": "https://web.archive.org/web/19990220045153/http://www1.umn.edu/"
+        },
+        {
+            "me": "./The Wayback Machine/UMN_MainPage.html",
+            "orig": "https://web.archive.org/web/19990220045153/http://www1.umn.edu/"
+        }
+    ]
+
+}
